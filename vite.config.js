4060a3915a919d70df036e43f8843b31c67526c8
jameygleason <james.p.gleason@gmail.com>
2023-04-21 18:20:26 -0400

    changed serve root to server

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vite"
 
 export default defineConfig({
-  root: "./",
-  server: {
-    port: 3000,
-  },
-});
+	root: "./src",
+	server: {
+		port: 3000,
+	},
+})
